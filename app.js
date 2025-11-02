b5abfa5c18eafdf9625d4f6430c965303368cd6c
vivekumartomar <ervivekumar01@gmail.com>
2025-11-02 16:05:58 +0530

    make a db connection string

--- app.js
+++ app.js
@@ -1,31 +1,15 @@
 const express = require("express");
+const { connectDB } = require('./config/database')
 
 const app = express();
 
-// to get the dynamic params
-app.use('/user', (req, res, next)=>{
-    console.log('1st route handler');
-    next();
-
-},(req, res, next) =>{
-    console.log('2nd route handler');
-    next();
-}, (req, res, next) =>{
-    console.log('3rd route handler');
-    next();
-}, (req, res, next) =>{
-    console.log('4th route handler');
-    // throw new Error("Something went wrong!");
-    res.send('done!!!');
-
-} , (error, req, res, next) =>{
-    console.log(error)
-    res.status(500).send("Something went wrong!");
-}
-
-)
-
-app.listen(7777, ()=>{
-    console.log("server startted at 7777");
+connectDB().then(() => {
+    console.log('connected to db');
+    app.listen(7777, ()=>{
+        console.log("server startted at 7777");
+    })
+}).catch((err) => {
+    console.log('something went wrong in db connection');
 })
+
 
