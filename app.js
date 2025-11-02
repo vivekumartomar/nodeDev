6e8b674a62f89b79593840c89cfefe29f133f8f6
vivekumartomar <ervivekumar01@gmail.com>
2025-11-02 15:32:42 +0530

    dynamic routes

--- app.js
+++ app.js
@@ -2,18 +2,25 @@ const express = require("express");
 
 const app = express();
 
+// to get the dynamic params
+app.use('/user/:userid', (req, res)=>{
+    console.log(req.params);
+    res.send('req.params'+ req.params.userid);
+})
+
+// /to gets query param
+app.get('/getuserdata', (req, res) => {
+console.log(req.query)
+   res.send(req.query)
+
+//    next();
+});
+
+// app.use('/', (err, req, res, next) => {
+//     res.status(500).send('something went wrong');
+// })
+
 app.listen(7777, ()=>{
     console.log("server startted at 7777");
 })
 
-app.use("/abc", (req, res) => {
-    res.send("hello world");
-});
-
-app.use("/test", (req, res) => {
-    res.send("hello from test world");
-});
-
-app.use("/type", (req, res) => {
-    res.send("hello world");
-});
